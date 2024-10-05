a36cc56d7ae3f98d593f948d140a414b7e6ede98
Zahran03 <zalfarizi502@gmail.com>
2024-10-05 21:23:34 +0700

    Create Combo box

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,63 @@
-import { serachManufacturerProps } from "@/types";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOptions,
+  Transition,
+} from "@headlessui/react";
 import React from "react";
+import { useState, Fragment } from "react";
+import Image from "next/image";
+
+import { manufacturers } from "@/constants";
+import { serachManufacturerProps } from "@/types";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: serachManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          {/* Button for the combobox */}
+          <ComboboxButton className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="car logo"
+            />
+          </ComboboxButton>
+
+          {/* Input Field for searching */}
+          <ComboboxInput
+            className="search-manufacturer__input"
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder="Volkswagen"
+          />
+
+          {/* Transition for display the options */}
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <ComboboxOptions
+              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              static
+            ></ComboboxOptions>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
