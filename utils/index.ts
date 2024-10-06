d7b9b3c57c4f79357b64110c1250295a9eff8f30
Zahran03 <zalfarizi502@gmail.com>
2024-10-06 20:09:50 +0700

    Created: fetchCars Function

---
+++ utils/index.ts
@@ -0,0 +1,32 @@
+// const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-key": "a0d63a94eamsh64195cecef77700p1d4ccajsnf8965ad19949",
+//     "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await fetch(url, options);
+//   const result = await response.text();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
+
+export async function fetchCars() {
+  const headers = {
+    "x-rapidapi-key": "a0d63a94eamsh64195cecef77700p1d4ccajsnf8965ad19949",
+    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+  const result = await response.json();
+  return result;
+}
